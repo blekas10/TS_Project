9634809ab1aa5d827a6cffab9aca295677443851
nojus <nojus@happyweb.dk>
2023-01-27 11:30:56 +0200

    fixed table errors

--- src/components/app.ts
+++ src/components/app.ts
@@ -2,6 +2,7 @@ import categories from "../data/categories";
 import products from "../data/products";
 import productsCategories from "../data/products-categories";
 import ProductsCollection from "../helpers/products-collection";
+import Table from "./table";
 
 
 class App {
@@ -31,7 +32,28 @@ class App {
     }
 
     initialize(){
-        this.htmlElement.innerHTML = `<div>cia bus app turinys</div>`;
+        
+        const table = new Table({
+            title:"test lent",
+            columns: {
+                name:'Pavadinimas',
+                price: 'kaina',
+                price1: '1kaina'
+            },
+            rowsData: [
+                {name:'Pavadinimas', price: '1',price1: 'kaina',},
+                {name:'Pavadinimas', price: '2',price1: 'kaina111',},
+                {name:'Pavadinimas', price: '3',price1: 'kaina222',},
+            ]
+        });
+
+        const container = document.createElement('div');
+        container.className = 'container my-4';
+        container.append(table.htmlElement);
+
+       
+        
+        this.htmlElement.append(container);
     }
 }
 
--- src/components/table.ts
+++ src/components/table.ts
@@ -1,5 +1,89 @@
-class Table {
 
+import getPropCount from "../helpers/get-prop-count";
+
+type rowData = {
+    [key:string]:string
+}
+
+type TableProps<Type extends rowData> = {
+    title: string,
+    columns: Type,
+    rowsData: Type[],
+}
+
+class Table<Type extends rowData> {
+
+    static propsAreValid = <T extends rowData>({columns, rowsData}: TableProps<T>): boolean => {
+        const colCount = getPropCount(columns);
+
+        return rowsData.every(row => getPropCount(row) === colCount)
+    };
+    
+    private props: TableProps<Type>;
+    private tbody: HTMLTableSectionElement;
+    private thead: HTMLTableSectionElement;
+    public htmlElement: HTMLTableElement;
+
+    constructor(props: TableProps<Type>){
+        if(!Table.propsAreValid(props)){
+            throw new Error('props are not compatable. Please check columns and rows data ')
+        }
+
+        this.props = props;
+        this.thead = document.createElement('thead');
+        this.tbody = document.createElement('tbody');
+        this.htmlElement = document.createElement('table');
+
+        this.initialize();
+    }
+
+    initializeHead = () => {
+        console.log(this.props.columns);
+
+        const thElementsString = Object.values(this.props.columns).map(columnName => `<th>${columnName}</th>`).join("")
+        
+        const columnCount = thElementsString.length;
+
+        this.thead.className = 'bg-dark text-white' 
+        this.thead.innerHTML = `
+        <tr class="text-center h2">
+            <th colspan="${columnCount}">${this.props.title}</th>
+        </tr>
+        <tr>
+            ${thElementsString}
+        </tr>
+        `;
+            
+    };
+
+    initializeBody = () => {
+        const trsHtmlString = this.props.rowsData.map((rowData) => {
+
+            const tdsHtmlStr= Object.keys(this.props.columns).map(key => `<td>${rowData[key]}</td>`).join("");
+
+            return `<tr>${tdsHtmlStr}</tr>`;
+        })
+        .join('');
+
+        console.log(trsHtmlString);
+        
+
+
+        this.tbody.innerHTML = trsHtmlString;
+    };
+
+
+    initialize(){
+        this.initializeHead();
+        this.initializeBody();
+
+        this.htmlElement.className = 'table table-striped'
+        
+        this.htmlElement.append(
+            this.thead,
+            this.tbody
+        )
+    }
 }
 
 export default Table;
